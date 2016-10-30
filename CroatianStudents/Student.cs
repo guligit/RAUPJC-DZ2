5f3669058fef3c32c533e46e891c7481ee0f5e62
guligit <gmaky14@gmail.com>
2016-10-30 23:42:19 +0100

    4. i 5. zadatak

---
+++ CroatianStudents/Student.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CroatianStudents
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Jmbag { get; set; }
+        public Gender Gender { get; set; }
+
+        public Student(string name, string jmbag)
+        {
+            Name = name;
+            Jmbag = jmbag;
+        }
+
+        public Student(string name, string jmbag, Gender gender)
+        {
+            Name = name;
+            Jmbag = jmbag;
+            Gender = gender;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Student objx = obj as Student;
+            if (ReferenceEquals(objx, null)) return false;
+            return (Name.Equals(objx.Name) && Jmbag.Equals(objx.Jmbag));
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() + Jmbag.GetHashCode();
+        }
+
+        public static bool operator ==(Student first, Student second)
+        {
+            if (ReferenceEquals(first, null))
+                return ReferenceEquals(second, null);
+
+            return first.Equals(second);
+        }
+
+        public static bool operator !=(Student first, Student second)
+        {
+            return !(first == second);
+        }
+    }
+}
+
---
+++ CroatianStudents/University.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Threading;
+
+namespace CroatianStudents
+{
+    public class University
+    {
+        public string Name { get; set; }
+        public Student[] Students { get; set; }
+        private int Count;
+
+        public University(string name)
+        {
+            Name = name;
+        }
+
+    }
+}
